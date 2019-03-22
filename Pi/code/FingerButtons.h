c2f3295d4ec75723061804bb9f8b9884935977ed
David Hallstrom <dhallstr@rams.colostate.edu>
2019-03-22 17:10:20 -0600

    Implemented FingerButtons

--- Pi/code/FingerButtons.h
+++ Pi/code/FingerButtons.h
@@ -1,6 +1,8 @@
 #ifndef FingerButtons_H_INCLUDED
 #define FingerButtons_H_INCLUDED
 
+#define NUM_FINGERS 10
+
 #include <iostream>
 #include <cstring>
 #include "Leap.h"
@@ -12,14 +14,14 @@ typedef struct fingerConfig_s {
     // and 1 (finger entirely normal to palm, anatomically impossible)
     // elements 0-4 represent left pinky through left thumb, and
     // 5-9 represent right thumb through right pinky.
-    double fingerSensitivities[10];
+    double fingerSensitivities[NUM_FINGERS];
 
 } fsensitivity_t;
 
 class FingerButtons {
     public:
         // call each time you receive a new frame
-        void updateFrame(Leap::Frame frame);
+        void updateFrame(const Leap::Frame &frame);
 
         // returns true if both hands are in the latest frame
         bool areHandsVisible() const;
@@ -31,10 +33,13 @@ class FingerButtons {
         bool checkButtonPresses() const;
 
         // change sensitivity settings
-        void setSensitivity(fsensitivity_t fs);
+        void setSensitivity(const fsensitivity_t &fs);
 
         // set the callback functions for each finger
-        void setCallback(void (* fns [10])(int));
+        void setCallback(void (* fns [NUM_FINGERS])(int));
+        
+        // set all callbacks to be a single function (determine which finger it was by the parameter)
+        void setAllCallbacks(void (*fn)(int));
 
         // I would write a method to get the callback methods so you can just set one, but I don't know how.
         // So I will instead provide this method, which allows you to modify the function pointer of finger id.
@@ -48,6 +53,6 @@ class FingerButtons {
 
         // Callback functions for each finger
         // (each function is void and takes a single int parameter, the fingerId 0-9)
-        void (* callbacks [10])(int);
+        void (* callbacks [NUM_FINGERS])(int);
 };
 #endif
