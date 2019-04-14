a93a0079f35d309a5093b7cd94a676ead6ab14a1
Lance McMullin <lance.mcmullin@gmail.com>
2019-04-13 18:23:33 -0600

    Initial ideas for Joysticks.h

--- Pi/code/Joysticks.h
+++ Pi/code/Joysticks.h
@@ -1,1 +1,45 @@
+#ifndef Joysticks_H_INCLUDED
+#define Joysticks_H_INCLUDED
 
+#include "Leap.h"
+
+typedef struct handConfig_s {
+    //diameter, in mm, of the circular area around the center of the joystick that does not respond to movement
+    double joystickDeadzone;
+    //height, in mm, that a hand must go below in order for a joystick button to be clicked
+    double handDepthSensitivity;
+
+} hsensitivity_t;
+
+class Joysticks
+{
+    public:
+        // call each time you receive a new frame
+        void updateFrame(const Leap::Frame &frame);
+
+        // returns true if both hands are in the latest frame
+        bool areHandsVisible() const;
+
+        // returns true if hand handLetter is considered "down" depending on the hsensitivity settings
+        bool isPressedDown(char handLetter) const;
+
+        // checks palms for joystick button presses and calls appropriate functions
+        bool checkJoystickPresses() const;
+
+        // change sensitivity settings
+        void setSensitivity(const hsensitivity_t &hs);
+               
+        // returns the x coordinate of the palm of the hand specified by handLetter
+        float getPalmX(char handLetter);
+        
+        // returns the y coordinate of the palm of the hand specified by handLetter
+        float getPalmY(char handLetter);
+    
+    private:
+    // call Hand::isValid() to see if the hands are in the frame or not
+        Leap::Hand leftHand, rightHand;
+
+        hsensitivity_t config;
+}
+
+#endif
