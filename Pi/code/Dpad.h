75701ed559687a92e7c26d0b3a052c7b5a9ba12a
Kellyn Dassler <36115559+kellyndassler@users.noreply.github.com>
2019-04-19 04:09:41 -0600

    Dpad.h
    Dpad.h file

---
+++ Pi/code/Dpad.h
@@ -0,0 +1,28 @@
+    
+#ifndef DPAD_H_INCLUDED
+#define DPAD_H_INCLUDED
+
+#include "Leap.h"
+
+class Dpad {
+    public:
+        // call each time you receive a new frame
+        void updateFrame(const Leap::Frame &frame);   
+
+        //returns true if d-pad pressed up
+        bool up(const Hand &hand) const;
+        
+        //returns true if d-pad pressed down
+        bool down(const Hand &hand) const;
+        
+        //returns true if d-pad pressed left
+        bool left(const Hand &hand) const;
+        
+        //returns true if d-pad pressed right
+        bool right(const Hand &hand) const;
+        
+        //for determining if hand tilt is occuring
+        bool tilt;	
+    //private:  
+};
+#endif
