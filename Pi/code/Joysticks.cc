3e7c7ba2a2c4bb1e10f7be2893798a56277aa176
Lance McMullin <lance.mcmullin@gmail.com>
2019-04-19 21:42:03 -0600

    Lance joystick branch (#17)
    * Created Joysticks.h
    
    * Created Joysticks.cc
    
    * Initial ideas for Joysticks.h
    
    * Update Joysticks.h
    
    * Update Joysticks.cc
    
    * Update Joysticks.h
    
    * Update Joysticks.h
    
    * Update Joysticks.h
    
    * Update Joysticks.cc
    
    * Update Joysticks.h
    
    * Made it so it compiled lul
    
    * Create testJoysticks.cc
    
    * Added joysticks test code
    
    * Updated offset strategy
    
    * Updated offset strategy
    
    * Updated offset strategy
    
    * Updated offset strategy
    
    * Updated offset strategy
    
    * Updated offset strategy
    
    * Updated offset strategy

---
+++ Pi/code/Joysticks.cc
@@ -0,0 +1,107 @@
+#include "Joysticks.h"
+#include <string>
+
+using namespace std;
+using namespace Leap;
+
+void Joysticks::updateFrame(const Leap::Frame &frame) //copied from FingerButtons.cc
+{
+    // remove past hands so if a hand was moved out of the frame, we don't consider it
+    leftHand = Hand::invalid();
+    rightHand = Hand::invalid();
+    
+    // look through hands and set the appropriate variables.
+    // if there are multiple left hands, for example, which one
+    // is chosen is unspecified (hands() uses an arbitrary ordering)
+    for (const Hand &h: frame.hands()) {
+        if (h.isValid()) {
+            if (h.isLeft()) {
+                leftHand = h;
+            }
+            else if (h.isRight()) {
+                rightHand = h;
+            }
+        }
+    }
+}
+
+bool Joysticks::areHandsVisible() const //copied from FingerButtons.cc
+{
+    return leftHand.isValid() && rightHand.isValid();
+}
+
+bool Joysticks::isPressedDown(char handLetter) const
+{  
+    if(handLetter == 'L' || handLetter == 'l')
+    {
+        if(!leftHand.isValid()) return false;
+    }
+    else if (handLetter == 'R' || handLetter == 'r') {
+       if(!rightHand.isValid()) return false;
+    }
+    else {
+        return false;
+    }
+  
+    double currHeight = getPalmCoord(handLetter, 'Y');
+  
+    return currHeight <= config.handDepthSensitivity;
+}
+
+void Joysticks::setSensitivity(const hsensitivity_t &hs)
+{
+    config = hs;
+}
+
+//float Joysticks::getJoystickVal(char handLetter, char axis, double palmCoord) const
+//{
+//	double 
+//	if (axis == 'X' || axis == 'x')
+//}
+ 
+float Joysticks::getPalmCoord(char handLetter, char axis) const
+{
+    Vector handPos;
+    int intAxis;
+	//double centerOffset;
+	int offsetIndex = 0;
+	double joystickVal;
+    
+    //determine what hand to look at
+    if		(handLetter == 'L' || handLetter == 'l')    handPos = leftHand.palmPosition();
+	else if (handLetter == 'R' || handLetter == 'r'){	handPos = rightHand.palmPosition(); offsetIndex += 2;}
+    else throw ("Error: could not determine hand from given hand letter: "s + handLetter);
+    
+    //determine what axis to access in the vector
+    if      (axis == 'X' || axis == 'x')     intAxis = 0;
+	else if (axis == 'Y' || axis == 'y') {   intAxis = 1; return handPos[intAxis]; }
+	else if (axis == 'Z' || axis == 'z') {	 intAxis = 2; offsetIndex += 1; }
+    else throw ("Error: could not determine axis from given letter: "s + axis);
+    
+
+	//if (axis == 'X' || axis == 'x')
+	//{
+	//	if ((handLetter == 'R' || handLetter == 'r') && (handPos[intAxis] < 0)) // is the right hand x negative?
+	//		return 128.0;
+	//	if ((handLetter == 'L' || handLetter == 'l') && (handPos[intAxis] > 0)) // is the left hand x positive?
+	//		return 128.0;
+	//}
+	if (handLetter == 'L' || handLetter == 'l')
+	{
+		if (!leftHand.isValid()) return 128.0;
+	}
+	else if (handLetter == 'R' || handLetter == 'r') {
+		if (!rightHand.isValid()) return 128.0;
+	}
+	else {
+		return 128.0;
+	}
+
+	joystickVal = handPos[intAxis] - config.palmOffsets[offsetIndex];
+
+	if (joystickVal > 255) joystickVal = 255;
+	else if (joystickVal < 0) joystickVal = 0;
+
+    return joystickVal;
+}
+
---
+++ Pi/code/Joysticks.h
@@ -0,0 +1,54 @@
+#ifndef Joysticks_H_INCLUDED
+#define Joysticks_H_INCLUDED
+
+#include <iostream>
+#include "Leap.h"
+
+typedef struct handConfig_s {
+    // diameter, in mm, of the circular area around the center of the joystick that does not respond to movement
+    double joystickDeadzone;
+    
+    // height, in mm, that a hand must go below in order for a joystick button to be clicked
+    double handDepthSensitivity;
+
+	double rangeOfMotionScalingFactor;
+    
+    // array of values representing the amount, in mm, to offset the palm position of each hand
+    // elements in the array are as follows:
+    //      0 : left hand x offset
+    //      1 : left hand z offset
+    //      2 : right hand x offset
+    //      3 : right hand z offset
+    double palmOffsets[4];
+
+} hsensitivity_t;
+
+class Joysticks
+{
+    public:
+        // call each time you receive a new frame
+        void updateFrame(const Leap::Frame &frame);
+
+        // returns true if both hands are in the latest frame
+        bool areHandsVisible() const;
+
+        // returns true if hand handLetter is considered "down" depending on the hsensitivity settings
+        bool isPressedDown(char handLetter) const;
+
+        // change sensitivity settings
+        void setSensitivity(const hsensitivity_t &hs);
+
+		// returns a value between 0-255 that represents how far the joystick is from its center
+		//float getJoystickVal(char handLetter, char axis, double palmCoord) const;
+               
+        // returns the coordinate of the palm of the hand specified by handLetter and on the axis given
+        float getPalmCoord(char handLetter, char axis) const;
+    
+    private:
+    // call Hand::isValid() to see if the hands are in the frame or not
+        Leap::Hand leftHand, rightHand;
+
+        hsensitivity_t config;
+};
+
+#endif
