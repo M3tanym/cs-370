ae7746b985afd4ee8567c19e898250ac8d9352d6
Kellyn Dassler <36115559+kellyndassler@users.noreply.github.com>
2019-04-19 19:04:54 -0600

    Update Dpad.cc

--- Pi/code/Dpad.cc
+++ Pi/code/Dpad.cc
@@ -25,9 +25,11 @@ void Dpad::updateFrame(const Leap::Frame &frame) {
     }           
 }
 
+
 //returns true if hand tilted "up" depending on pitch position (gets tilt around x axis)
 //pitch returns negative value if in "up" direction (I think--need to test with leap to make sure)
-bool Dpad::up(const Hand &hand) const {
+bool Dpad::up(Hands h) const {
+    Hand hand = whichHand(h);
     float pitch = hand.direction().pitch();
     
     if(pitch >= 0 && pitch <=180){
@@ -43,7 +45,8 @@ bool Dpad::up(const Hand &hand) const {
 
 //returns true if hand tilted "down" depending on pitch position (gets tilt around x axis)
 //pitch returns positive value if in "down" direction (I think--need to test with leap to make sure)
-bool Dpad::down(const Hand &hand) const {
+bool Dpad::down(Hands h) const {
+    Hand hand = whichHand(h);
     float pitch = hand.direction().pitch();
     
     if(pitch >= 0 && pitch <=180){
@@ -59,7 +62,8 @@ bool Dpad::down(const Hand &hand) const {
 
 //returns true if hand tilted "left" depending on roll position (gets tilt around z axis)
 //roll returns negative value if in "left" direction (I think--need to test with leap to make sure)
-bool Dpad::left(const Hand &hand) const {
+bool Dpad::left(Hands h) const {
+    Hand hand = whichHand(h);
     float roll = hand.palmNormal().roll();
     
     if(roll >= 0 && roll <=180){
@@ -76,7 +80,8 @@ bool Dpad::left(const Hand &hand) const {
 
 //returns true if hand tilted "right" depending on roll position (gets tilt around z axis)
 //roll returns positive value if in "left" direction (I think--need to test with leap to make sure)
-bool Dpad::right(const Hand &hand) const {
+bool Dpad::right(Hands h) const {
+    Hand hand = whichHand(h);
     float roll = hand.palmNormal().roll();
     
     if(roll >= 0 && roll <=180){
@@ -88,4 +93,14 @@ bool Dpad::right(const Hand &hand) const {
     else {
         return false;
     }
+}
+
+//return which Hand to use for dpad
+Hand Dpad::whichHand(Hands h){
+     Hand hand;
+     if(h==0)
+          hand = leftHand;
+     else
+          hand = rightHand;
+     return hand;
 }
