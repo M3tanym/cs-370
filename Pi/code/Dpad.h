7f15f4f902b9a722014e88a142b1c48e79c85e40
Kellyn Dassler <36115559+kellyndassler@users.noreply.github.com>
2019-04-19 19:18:10 -0600

    Dpad code (#15)
    * Dpad.cc
    
    Dpad.cc--need to test, and check edge cases/update
    
    * Update Dpad.cc
    
    * Dpad.h
    
    Dpad.h file
    
    * Update Dpad.h
    
    Updated left and right hand variables for calling hands.
    
    * Update Dpad.h
    
    * Update Dpad.cc
    
    * Update Dpad.h
    
    * Update Dpad.h
    
    * Update Dpad.cc
    
    * Create testDpad.cc

---
+++ Pi/code/Dpad.h
@@ -0,0 +1,32 @@
+    
+#ifndef DPAD_H_INCLUDED
+#define DPAD_H_INCLUDED
+
+#include "Leap.h"
+
+class Dpad {
+    public:
+    
+        //type Hands
+        enum Hands{leftHand = 0, rightHand = 1};
+    
+        // call each time you receive a new frame
+        void updateFrame(const Leap::Frame &frame);   
+    
+        //returns true if d-pad pressed up
+        bool up(Hands h) const;
+        
+        //returns true if d-pad pressed down
+        bool down(Hands h) const;
+        
+        //returns true if d-pad pressed left
+        bool left(Hands h) const;
+        
+        //returns true if d-pad pressed right
+        bool right(Hands h) const;
+    private:
+        Leap::Hand Dpad::whichHand(Hands h)
+        Leap::Hand leftHand, rightHand;
+        
+};
+#endif
