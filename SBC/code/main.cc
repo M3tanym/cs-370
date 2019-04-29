137332d4d7dd05708091d2ec3e378579cb581cd6
Ben Gillett <bnjmn.gillett@gmail.com>
2019-04-28 19:11:33 -0600

    tweaked sensitivities

--- SBC/code/main.cc
+++ SBC/code/main.cc
@@ -150,10 +150,10 @@ int main(int argc, char **argv)
 	jsens.handDepthSensitivity = 300;
 	jsens.joystickSensitivity = 2;
 	jsens.palmOffsets[0] = -300;  //      0 : left hand x offset
-	jsens.palmOffsets[1] = 100;  //      1 : left hand y offset
+	jsens.palmOffsets[1] = 150;  //      1 : left hand y offset
 	jsens.palmOffsets[2] = -128;  //      2 : left hand z offset
 	jsens.palmOffsets[3] = 0;  //      3 : right hand x offset
-	jsens.palmOffsets[4] = 100;  //      4 : right hand y offset
+	jsens.palmOffsets[4] = 150;  //      4 : right hand y offset
 	jsens.palmOffsets[5] = -128; //      5 : right hand z offset
 	joysticks.setSensitivity(jsens);
 
